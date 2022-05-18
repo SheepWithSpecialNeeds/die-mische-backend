0b5542d959c24fa6e0feeff8290f210da7a9ace6
SheepWithSpecialNeeds <77433967+SheepWithSpecialNeeds@users.noreply.github.com>
2022-05-18 11:01:55 +0200

    Init Spring Project

---
+++ src/main/java/de/kiraroth/diemische/DieMischeApplication.java
@@ -0,0 +1,13 @@
+package de.kiraroth.diemische;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieMischeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieMischeApplication.class, args);
+	}
+
+}
